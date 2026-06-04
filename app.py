3d2a979f0821fafd5fbec8e22422676e6993afbf
近藤 哲 <kondosatoru@kondounoMacBook-Air.local>
2026-06-04 23:41:16 +0900

    check secrets

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
 import streamlit as st
-import gspread
-from google.oauth2.service_account import Credentials
-import pandas as pd
-
-st.set_page_config(page_title="不動産ダッシュボード", layout="wide")
 
 st.write("アプリ開始")
 
-# ======================
-# Google認証
-# ======================
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
+try:
+    st.write("Secrets確認開始")
 
-st.write("認証前")
+    gcp = st.secrets["gcp_service_account"]
 
-creds = Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=scope
-)
+    st.write("Secrets取得成功")
 
-st.write("認証成功")
+    st.write(gcp["project_id"])
 
-client = gspread.authorize(creds)
-
-st.write("gspread成功")
-
-spreadsheet = client.open("不動産管理DB")
-
-st.write("スプレッドシート接続成功")
+except Exception as e:
+    st.error(e)
+    st.stop()
