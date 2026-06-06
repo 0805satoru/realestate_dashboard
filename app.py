6dac61d5dc62cadcb61ba9480933d85a1543a511
近藤 哲 <kondosatoru@kondounoMacBook-Air.local>
2026-06-06 12:26:02 +0900

    debug startup

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ st.markdown(
 with st.container(border=True):
     st.markdown(
         f"""
-        **物件数：** {total_properties}件 ｜ 
-        **総戸数：** {total_units}戸 ｜ 
-        **入居率：** {occupancy_rate:.0f}%
+        **物件数:** {total_properties}件 ｜ 
+        **総戸数:** {total_units}戸 ｜ 
+        **入居率:** {occupancy_rate:.0f}%
         """
     )
 
