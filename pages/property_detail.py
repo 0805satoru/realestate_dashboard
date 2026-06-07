2d8b4a465c72d22263ac4b9dff1a05dddad7c96d
近藤 哲 <kondosatoru@kondounoMacBook-Air.local>
2026-06-07 19:08:11 +0900

    debug startup

--- pages/property_detail.py
+++ pages/property_detail.py
@@ -94,30 +94,17 @@ with tab1:
     for i, row in property_rooms.iterrows():
         with st.container(border=True):
 
-            st.markdown(
-                f"""
-                <div style="
-                    display:flex;
-                    justify-content:space-between;
-                    align-items:center;                    ">
-                    <span style="font-size:20px; font-weight:700;">
-                        {row['room']}号室
-                    </span>
-
-                    <span style="font-size:18px; font-weight:600;">
-                        {row['total_rent']}
-                    </span>
-                </div>
-                """,                    unsafe_allow_html=True
-            )
-
-
-
             col1, col2 = st.columns([2, 1])
             with col1:
                 st.markdown(
-                f"<div style='font-size:18px; font-weight:600;'>"
+                f"<div style='font-size:20px; font-weight:700;'>"
                 f"{row['room']}号室"
+                f"</div>",
+            unsafe_allow_html=True
+            )
+                st.markdown(
+                f"<div style='font-size:18px; font-weight:600;'>"
+                f"💰 {row['total_rent']}円/月"
                 f"</div>",
             unsafe_allow_html=True
             )
