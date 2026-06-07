88c207b0755265324652613da787679b26c204a9
近藤 哲 <kondosatoru@kondounoMacBook-Air.local>
2026-06-07 23:17:23 +0900

    debug startup

--- pages/property_detail.py
+++ pages/property_detail.py
@@ -84,10 +84,10 @@ tab1, tab2 = st.tabs([
 ])
 
 with tab1:
-    #st.markdown(
-    #    f"<div style='font-size:20px; font-weight:700;'>部屋情報</div>",
-    #    unsafe_allow_html=True
-    #)
+    st.markdown(
+        f"<div style='font-size:20px; font-weight:700;'>部屋情報</div>",
+        unsafe_allow_html=True
+    )
 
     for i, row in property_rooms.iterrows():
         with st.container(border=True):
