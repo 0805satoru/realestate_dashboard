1c61df0a0362a126a052c287a0924af29462deb8
近藤 哲 <kondosatoru@kondounoMacBook-Air.local>
2026-06-08 23:20:32 +0900

    debug startup

--- pages/property_detail.py
+++ pages/property_detail.py
@@ -145,9 +145,6 @@ with tab2:
 
     vacancy_loss = full_income - current_income
 
-    st.write(current_income)
-    st.write(type(current_income))
-
     st.markdown(
         f"""
         <div style='font-size:18px; font-weight:600;'>
